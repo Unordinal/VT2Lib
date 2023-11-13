cf3335f9a86b7238082939584f88a1f98011fa6d
Unordinal <unordinal@gmail.com>
2023-11-12 19:45:37 -0800

    Clean up .old files.
    some OLD_DATA here...

--- VT2Lib.Core/Stingray/IO/Resources/Writers/.old/Binary/IBinaryResourceWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using VT2Lib.Core.Stingray.IO.Resources.Writers;
-using VT2Lib.Core.Stingray.Resources;
-
-namespace VT2Lib.Core.Stingray.IO.Resources.Writers.Binary;
-
-public interface IBinaryResourceWriter : IResourceWriter
-{
-    void Write(BinaryWriter writer, IResource resource);
-}
